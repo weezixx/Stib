24b6c922bec09f8f090f21c74c7e150eccd07251
misterwhy <weezixx@hotmail.Com>
2021-07-04 19:50:38 +0200

    rien à changé

--- script.py
+++ script.py
@@ -3,6 +3,8 @@ import requests
 import base64
 import json
 
+# TODO : faire la différence entre le temps affiché et actuel (real time)
+
 
 def encode(message):
 
@@ -12,7 +14,7 @@ def encode(message):
 
     base64_message = base64_bytes.decode('ascii')
 
-    print(base64_message)
+    #print(base64_message)
 #
 #
 #
@@ -25,36 +27,43 @@ consumer_key_64 = str(encode(consumer_key))
 consumer_secret = "kucY0Z71BKyubZxJPPQFQMeWhn0a"
 #
 consumer_secret_64 = str(encode(consumer_secret))
-#
-# headers = {"grant_type":"client_credentials","Authorization":"Basic "+consumer_key_64+":"+consumer_secret_64}
-#
-#
-# res = requests.get(url)
-#
-# resp = requests.get(url,headers=headers)
-#
-# print(resp)
-#
-# t = requests.get(url_token,headers)
+
 
 message = 'https://opendata-api.stib-mivb.be/OperationMonitoring/4.0/MessageByLine/8031'
 
-url = 'https://opendata-api.stib-mivb.be/OperationMonitoring/4.0/PassingTimeByPoint/1746'
+time = 'https://opendata-api.stib-mivb.be/OperationMonitoring/4.0/PassingTimeByPoint/1746'
 
 #url = 'https://opendata-api.stib-mivb.be/NetworkDescription/4.0/PassingTimeByPoint/8031'
 
-headers = {'Accept': 'application/json','Authorization':'Bearer b9d31f3c844f31574127420628a76a58'}
+headers = {'Accept': 'application/json','Authorization':'Bearer  80e5da470868266c4ac70c269f6fc02f '}
 
 #headers = {"grant_type":"client_credentials","Authorization":"Basic "+consumer_key_64+":"+consumer_secret_64",'https://opendata-api.stib-mivb.be/token'}
 
-resp = requests.get(url,headers=headers)
+resp = requests.get(time,headers=headers)
+
+info = requests.get(message,headers=headers)
 
 #req_json = requests.get(url, headers=headers)
 
 #req_python = json.loads(req_json.text)
 
+get = resp.text
+
 print(resp.text)
+
+print(info.text)
+
+get = json.loads(resp.text)
+
+# numéro de ligne + destination
+print(get['points'][0]['passingTimes'][0]['lineId'], get['points'][0]['passingTimes'][0]['destination']['fr'])
+print(get['points'][0]['passingTimes'][0]['expectedArrivalTime'])
+
+print(get['points'][1]['passingTimes'][0]['lineId'], get['points'][1]['passingTimes'][0]['destination']['fr'])
+print(get['points'][1]['passingTimes'][0]['expectedArrivalTime'])
+
+print(len(get))
 
 t = requests.get(url_token,headers)
 
-print(t)
+#print(t)
